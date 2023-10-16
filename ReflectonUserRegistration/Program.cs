e26914cc08432802d3254076072f6b2c2dcc2908
kaidathulipriyanka <131505554+kaidathulipriyanka@users.noreply.github.com>
2023-10-16 22:03:05 +0530

    Program.cs

--- ReflectonUserRegistration/Program.cs
+++ ReflectonUserRegistration/Program.cs
@@ -5,23 +5,21 @@
         static void Main(string[] args)
         {
             UserRegistration registration = new UserRegistration();
-            string firstName = "Battu";
-            string lastName = "Sairam";
-            string email = "Battusairam@gmail.com";
-            string mobile = "91 7036703736";
-            string password = "P@ssw0rd123";
+            string firstName = "Priya";
+            string lastName = "prasanya";
+            string email = "Kaidhathulipriyanka@gmail.com";
+            string mobile = "91 8179664954";
+            string password = "priya@123";
 
-            // Test each validation rule using reflection.
             bool isValidFirstName = registration.ValidateAllRules(registration, "ValidateFirstName", firstName);
+             Console.WriteLine("First Name is Valid: " + isValidFirstName);
             bool isValidLastName = registration.ValidateAllRules(registration, "ValidateLastName", lastName);
+             Console.WriteLine("Last Name is Valid: " + isValidLastName);
             bool isValidEmail = registration.ValidateAllRules(registration, "ValidateEmail", email);
-            bool isValidMobile = registration.ValidateAllRules(registration, "ValidateMobile", mobile);
-            bool isValidPassword = registration.ValidateAllRules(registration, "ValidatePassword", password);
-
-            Console.WriteLine("First Name is Valid: " + isValidFirstName);
-            Console.WriteLine("Last Name is Valid: " + isValidLastName);
             Console.WriteLine("Email is Valid: " + isValidEmail);
+            bool isValidMobile = registration.ValidateAllRules(registration, "ValidateMobile", mobile);
             Console.WriteLine("Mobile is Valid: " + isValidMobile);
+            bool isValidPassword = registration.ValidateAllRules(registration, "ValidatePassword", password);
             Console.WriteLine("Password is Valid: " + isValidPassword);
         }
     }
