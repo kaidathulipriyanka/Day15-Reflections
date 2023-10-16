fca377b01c6b867498f46664e855f0f43f65ea3b
kaidathulipriyanka <131505554+kaidathulipriyanka@users.noreply.github.com>
2023-10-16 21:55:45 +0530

    [priya] : ReflectionsUser-Registration

---
+++ ReflectonUserRegistration/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReflectonUserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration registration = new UserRegistration();
+            string firstName = "Battu";
+            string lastName = "Sairam";
+            string email = "Battusairam@gmail.com";
+            string mobile = "91 7036703736";
+            string password = "P@ssw0rd123";
+
+            // Test each validation rule using reflection.
+            bool isValidFirstName = registration.ValidateAllRules(registration, "ValidateFirstName", firstName);
+            bool isValidLastName = registration.ValidateAllRules(registration, "ValidateLastName", lastName);
+            bool isValidEmail = registration.ValidateAllRules(registration, "ValidateEmail", email);
+            bool isValidMobile = registration.ValidateAllRules(registration, "ValidateMobile", mobile);
+            bool isValidPassword = registration.ValidateAllRules(registration, "ValidatePassword", password);
+
+            Console.WriteLine("First Name is Valid: " + isValidFirstName);
+            Console.WriteLine("Last Name is Valid: " + isValidLastName);
+            Console.WriteLine("Email is Valid: " + isValidEmail);
+            Console.WriteLine("Mobile is Valid: " + isValidMobile);
+            Console.WriteLine("Password is Valid: " + isValidPassword);
+        }
+    }
+}
---
+++ ReflectonUserRegistration/UserRegistration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ReflectonUserRegistration
+{
+    public class UserRegistration
+    {
+       
+            public bool ValidateFirstName(string firstName)
+            {
+                 return Regex.IsMatch(firstName, "^[A-Z][a-z]{2,}");
+            }
+            public bool ValidateLastName(string lastName)
+            {
+                 return Regex.IsMatch(lastName, "^[A-Z][a-z]{2,}");
+            }
+            public bool ValidateEmail(string email)
+            {
+                 return Regex.IsMatch(email, @"^[a-zA-Z0-9]+([.+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$");
+            }
+
+            public bool ValidateMobile(string mobile)
+            {
+                 return Regex.IsMatch(mobile, @"^\d{2}\s\d{10}$");
+            }
+
+            public bool ValidatePassword(string password)
+            {
+                 return Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$");
+            }
+
+            public bool ClearEmailSamples(string email)
+            { 
+                return true;
+            }
+
+            public bool ValidateAllRules(object user, string methodName, string value)
+            {
+                 MethodInfo method = typeof(UserRegistration).GetMethod(methodName);
+                if (method != null)
+                {
+                    object result = method.Invoke(user, new object[] { value });
+                    return (bool)result;
+                }
+                return false;
+            }
+
+        }
+    }
